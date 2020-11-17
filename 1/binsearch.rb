ccd6c00081f55b5a687bd7772c5dd7e4cd432ef1
reoh <1750913582@qq.com>
2020-11-17 21:51:09 +0800

    binsearch

--- 1/binsearch.rb
+++ 1/binsearch.rb
@@ -1,17 +1,22 @@
-a = [9,1,8,2,7,3,6,4,5]
-x = 5
-a.sort!
-low = 1
-high = a.length
-location = 0
-while low <= high && location == 0
-  mid = (low+high)/2
-  if x==a[mid-1]
-    location = mid
-  elsif x<a[mid-1]
-    high = mid - 1
-  else
-    low = mid + 1
+def binsearch(s,x)
+  low = 1
+  high = s.length
+  location = 0
+  while low <= high && location == 0
+    mid = (low+high)/2
+    if x == s[mid-1]
+      location = mid
+    elsif x < [mid-1]
+      high = mid - 1
+    else
+      low = mid + 1
+    end
   end
+  puts location
 end
-puts location
+
+S = [9,1,8,2,7,3,6,4,5]
+x = 5
+S.sort!
+
+binsearch(S,x)
