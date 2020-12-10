7100439d755e9b4b74368558e989206d51ba0ad0
reoh <1750913582@qq.com>
2020-12-10 19:01:03 +0800

    mergesort

--- 2/mergesort.rb
+++ 2/mergesort.rb
@@ -4,37 +4,39 @@ def merge(array_u, array_v)
   i,j,k = 0,0,0
   u = array_u.length - 1
   v = array_v.length - 1
-  s = Array.new
+  array_s = Array.new
 
   while i <= u && j <= v
     if array_u[i] < array_v[j]
-      s << array_u[i]
+      array_s << array_u[i]
       i += 1
     else
-      s << array_v[j]
+      array_s << array_v[j]
       j += 1
     end
     k += 1
   end
 
   if i > u
-    s.concat(array_v[j..-1])
+    array_s.concat(array_v[j..-1])
   else
-    s.concat(array_u[i..-1])
+    array_s.concat(array_u[i..-1])
   end
 end
 
-def mergesort(s)
-  return s if s.length <= 1
+def mergesort(array_s)
+  if array_s.length > 1
+    left  = array_s.take(array_s.size/2)
+    right = array_s.drop(array_s.size/2)
 
-  left  = s.take(s.size/2)
-  right = s.drop(s.size/2)
+    sorted_left  = mergesort(left)
+    sorted_right = mergesort(right)
 
-  sorted_left  = mergesort(left)
-  sorted_right = mergesort(right)
-
-  merge(sorted_left, sorted_right)
+    merge(sorted_left, sorted_right)
+  else
+    return array_s
+  end
 end
 
-s_array = [27, 10, 12, 20, 25, 13, 15, 22]
-p mergesort(s_array)
+s = [27, 10, 12, 20, 25, 13, 15, 22]
+p mergesort(s)
