1e7bc695ff9cd3a74136deb90dc33d48a91a0bbb
reoh <1750913582@qq.com>
2020-12-10 18:03:41 +0800

    inplace mergesort

--- 2/mergesort_inplace.rb
+++ 2/mergesort_inplace.rb
@@ -1,38 +1,33 @@
-$s_array = [27, 10, 12, 20, 25, 13, 15, 22]
-$n       = $s_array.length
+$S = [27, 10, 12, 20, 25, 13, 15, 22]
 
 def merge(low, mid, high)
-  memory = Array.new(high+1)
-  i      = low
-  j      = mid + 1
-  k      = low
+  u = Array.new(high+1)
+  i,k = low,low
+  j = mid + 1
+
   while i <= mid && j <= high
-    if $s_array[i] < $s_array[j]
-      memory[k] = $s_array[i]
+    if $S[i] < $S[j]
+      u[k] = $S[i]
       i += 1
     else
-      memory[k] = $s_array[j]
+      u[k] = $S[j]
       j += 1
     end
     k += 1
   end
   if i > mid
-    $s_array[j..high].each do |n|
-      memory[k] = n
+    $S[j..high].each do |n|
+      u[k] = n
       k += 1
     end
   else
-    $s_array[i..mid].each do |n|
-      memory[k] = n
+    $S[i..mid].each do |n|
+      u[k] = n
       k += 1
     end
   end
-  $s_array[low..high] = memory[low..high]
-  p $s_array
-  p memory
+  $S[low..high] = u[low..high]
 end
-
-# merge(0, 3, 7)
 
 def mergesort(low, high)
   if low < high
@@ -43,4 +38,4 @@ def mergesort(low, high)
   end
 end
 
-mergesort(0, $n-1)
+p mergesort(0, $S.size-1)
