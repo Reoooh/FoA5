467b4881b4073334b27c4e308e214d304faf05db
reoh <1750913582@qq.com>
2020-12-29 03:14:51 +0800

    prod

--- 2/large_integer.rb
+++ 2/large_integer.rb
@@ -6,9 +6,9 @@ def prod(u, v)
     u * v
   else
     m = n / 2
-                            # 针对负数情况
-    x = u.quo(10**m).to_i   # Rational类商转换为Int类
-    y = u.remainder(10**m)  # 与被除数符号相同的余数
+
+    x = u.quo(10**m).to_i   # 针对可能负数情况：Rational类商转换为Int类
+    y = u.remainder(10**m)  # 取与被除数符号相同的余数
     w = v.quo(10**m).to_i
     z = v.remainder(10**m)
 
@@ -16,6 +16,29 @@ def prod(u, v)
   end
 end
 
+def prod2(u, v)
+  n = [u.abs.to_s.length, v.abs.to_s.length].max
+  if u == 0 || v == 0
+    0
+  elsif n <= 2
+    u * v
+  else
+    m = n / 2
+
+    x = u.quo(10**m).to_i
+    y = u.remainder(10**m)
+    w = v.quo(10**m).to_i
+    z = v.remainder(10**m)
+
+    r = prod2(x + y, w + z)
+    p = prod2(x, w)
+    q = prod2(y, z)
+
+    p * (10**(2 * m)) + (r - p - q) * 10**m + q
+  end
+end
+
 a = 567_832
-b = 9_423_723
+b = -9_423_723
 p prod(a, b)
+p prod2(a, b)
