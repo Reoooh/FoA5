224a510535e1c9ec4b53bf69fe9d272bd2ff7866
reoh <1750913582@qq.com>
2021-01-10 00:48:05 +0800

    floyd

--- 3/floyd.rb
+++ 3/floyd.rb
@@ -1,44 +1,44 @@
-def floyd(w)
-  n = w.size - 1
-  d = []
-  w.each { |m| d << m.clone }
+def floyd(matrix_w)
+  n = matrix_w.size - 1
+  matrix_d = []
+  matrix_w.each { |m| matrix_d << m.clone }
 
   (0..n).each do |k|
     (0..n).each do |i|
       (0..n).each do |j|
-        d[i][j] = [d[i][j], d[i][k] + d[k][j]].min
+        matrix_d[i][j] = [matrix_d[i][j], matrix_d[i][k] + matrix_d[k][j]].min
       end
     end
   end
 
-  d
+  matrix_d
 end
 
-def floyd2(w)
-  n = w.size - 1
-  p = Array.new(n + 1) { Array.new(n + 1) { 0 } }
-  d = []
-  w.each { |m| d << m.clone }
+def floyd2(matrix_w)
+  n = matrix_w.size - 1
+  matrix_p = Array.new(n + 1) { Array.new(n + 1) { 0 } }
+  matrix_d = []
+  matrix_w.each { |m| matrix_d << m.clone }
 
   (0..n).each do |k|
     (0..n).each do |i|
       (0..n).each do |j|
-        if d[i][k] + d[k][j] < d[i][j]
-          p[i][j] = k + 1
-          d[i][j] = d[i][k] + d[k][j]
+        if matrix_d[i][k] + matrix_d[k][j] < matrix_d[i][j]
+          matrix_p[i][j] = k + 1
+          matrix_d[i][j] = matrix_d[i][k] + matrix_d[k][j]
         end
       end
     end
   end
 
-  p
+  matrix_p
 end
 
 def path(q, r)
-  if $p[q - 1][r - 1] != 0
-    path(q, $p[q - 1][r - 1])
-    puts "v#{$p[q - 1][r - 1]}"
-    path($p[q - 1][r - 1], r)
+  if $P[q - 1][r - 1] != 0
+    path(q, $P[q - 1][r - 1])
+    puts "v#{$P[q - 1][r - 1]}"
+    path($P[q - 1][r - 1], r)
   end
 end
 
@@ -47,5 +47,5 @@ adjacency_matrix = [[0, 1, infinite, 1, 5], [9, 0, 3, 2, infinite], [infinite, i
 p floyd(adjacency_matrix)
 p floyd2(adjacency_matrix)
 
-$p = [[0, 0, 4, 0, 4], [5, 0, 0, 0, 4], [5, 5, 0, 0, 4], [5, 5, 0, 0, 0], [0, 1, 4, 1, 0]]
+$P = [[0, 0, 4, 0, 4], [5, 0, 0, 0, 4], [5, 5, 0, 0, 4], [5, 5, 0, 0, 0], [0, 1, 4, 1, 0]]
 path(5, 3)
