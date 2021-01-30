8b90cdfd0f2571e3f89ba16f13cb007f49597a0d
reoh <1750913582@qq.com>
2021-01-31 05:36:13 +0800

    prim&kruskal

--- 4/mimimum_spanning_tree.rb
+++ 4/mimimum_spanning_tree.rb
@@ -1,3 +1,4 @@
+# prim
 def prim(array_w)
   array_f  = []
   nearest  = []
@@ -34,6 +35,55 @@ def prim(array_w)
   array_f
 end
 
+# kruskal
+def initial(n)
+  Array(0..n - 1)
+end
+
+def find(i, u)
+  j = i
+  j = u[j] while u[j] != j
+  j
+end
+
+def merge(set_pointer_p, set_pointer_q, u)
+  if set_pointer_p < set_pointer_q
+    u[set_pointer_q] = set_pointer_p
+  else
+    u[set_pointer_p] = q
+  end
+end
+
+def kruskal(array_w)
+  n = array_w.length
+  array_m = []
+  (0..n - 1).each do |i|
+    (i + 1..n - 1).each do |j|
+      array_m << [array_w[i][j], [i, j]] if array_w[i][j] != Float::INFINITY
+    end
+  end
+  array_m.sort!
+
+  array_u = initial(n)
+
+  array_f = []
+  while array_f.length < n - 1
+    e = array_m[0]
+    i = e[1][0]
+    j = e[1][1]
+    sp_p = find(i, array_u)
+    sp_q = find(j, array_u)
+    if sp_p != sp_q
+      merge(sp_p, sp_q, array_u)
+      array_f << "v#{i + 1} -> v#{j + 1}"
+    end
+    array_m.shift
+  end
+
+  array_f
+end
+
 infinite = Float::INFINITY
 adjacency_matrix = [[0, 1, 3, infinite, infinite], [1, 0, 3, 6, infinite], [3, 3, 0, 4, 2], [infinite, 6, 4, 0, 5], [infinite, infinite, 2, 5, 0]] # 连通加权无向图的邻接矩阵
 p prim(adjacency_matrix)
+p kruskal(adjacency_matrix)
