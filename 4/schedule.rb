baa591c6e5ace1e17c84f19add40226f3222aca5
reoh <1750913582@qq.com>
2021-02-13 04:01:25 +0800

    schedule

--- 4/schedule.rb
+++ 4/schedule.rb
@@ -1,13 +1,13 @@
 def schedule(array_d)
   n = array_d.length
-  array_k = []
-  array_j = [1]
+  array_k = []  # 验证序列
+  array_j = [1] # 最优序列
 
   (2..n).each do |i|
-    array_k = array_j.clone
+    array_k = array_j.sort.clone
     array_k << i
 
-    feasibility_check = []
+    feasibility_check = [] # 可行性检测
     array_k.each { |j| feasibility_check << [j, array_d[j - 1]] }
     feasibility_check.sort! { |x, y| x[1] <=> y[1] }
 
@@ -15,11 +15,14 @@ def schedule(array_d)
     feasibility_check.length.times do |m|
       easibility = false if m + 1 > feasibility_check[m][1]
     end
-    array_j = array_k.clone if easibility
+    if easibility
+      array_j.clear
+      feasibility_check.each { |k| array_j << k[0] }
+    end
   end
 
-  array_j
+  p array_j
 end
 
 deadline = [3, 1, 1, 3, 3, 1, 3, 2] # 在标记任务前，已经对其进行了排序
-p schedule(deadline)
+schedule(deadline)
