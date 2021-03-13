ed65cc67b487e0ba51052c6ee33c92f8c64e6111
reoh <1750913582@qq.com>
2021-03-14 02:34:04 +0800

    queens

--- 5/queens.rb
+++ 5/queens.rb
@@ -1,10 +1,10 @@
 def queens(i)
   if promising(i)
     if i == $n - 1
-      p $col
+      puts "#{$col}"
     else
-      (0..$n - 1).each do |j|
-        $col[i + 1] = j
+      (1..$n).each do |j|
+        $col[i + 1] = j - 1
         queens(i + 1)
       end
     end
@@ -14,6 +14,7 @@ end
 def promising(i)
   k = 0
   switch = true
+
   while k < i && switch
     switch = false if $col[i] == $col[k] || ($col[i] - $col[k]).abs == i - k
     k += 1
@@ -23,4 +24,4 @@ end
 
 $n   = 4    # 暂令 n = 4
 $col = [0]  # col 与 n 非递归输入，因此定为全局变量
-queens(-1)
+queens(-1)  # -1 作为顶级调用参数
