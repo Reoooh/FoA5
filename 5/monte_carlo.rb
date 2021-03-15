d038e481e585ae0fb8047a0937e9de50a018ae6a
reoh <1750913582@qq.com>
2021-03-15 17:26:06 +0800

    estimate_n_queens

---
+++ 5/monte_carlo.rb
@@ -0,0 +1,40 @@
+def estimate_n_queens(n)
+  col = [0]
+  i = 0
+  numnodes = 1
+  m = 1
+  mprod = 1
+
+  while m != 0 && i != n
+    mprod *= m
+    numnodes += mprod * n
+    i += 1
+    m = 0
+    prom_children = []
+
+    (1..n).each do |j|
+      col[i - 1] = j - 1
+      if promising(i - 1, col)
+        m += 1
+        prom_children |= [j - 1]
+      end
+    end
+    if m != 0
+      j = prom_children.sample
+      col[i - 1] = j
+    end
+  end
+  numnodes
+end
+
+def promising(i, col)
+  k = 0
+  switch = true
+  while k < i && switch
+    switch = false if col[i] == col[k] || (col[i] - col[k]).abs == i - k
+    k += 1
+  end
+  switch
+end
+
+p estimate_n_queens(4)
